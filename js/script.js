02b26c8b23dabccf0c72e7600a050a56269a202e
Safi <safikurrahman7514@gmail.com>
2022-03-07 02:12:52 +0600

    added card container

--- js/script.js
+++ js/script.js
@@ -1,22 +1,41 @@
-// // elementById 
-// const elementId = (id) => {
-//     const elementIdName = document.getElementById(id)
-//     return elementIdName.value
-// }
+const elementId = (id) => {
+    const getId = document.getElementById(id)
+    return getId
+}
 
+const searchHandler = () => {
+    const keyword = elementId('keyword')
+    // console.log(keyword)
+    const url = (`https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`)
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => artistsData(data))
+}
 
-// // searchHandler button
-// const searchHandler = () => {
-//     const keyword = elementId('keyword');
-//     const url = (`https://www.theaudiodb.com/api/v1/json/2/search.php?s=${keyword}`)
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => showArtist(data))
-// }
-
-// const showArtist = (artists) => {
-//     console.log(artists)
-//     artists.map((artist) => {
-//         console.log(artist)
-//     })
-// }
+const artistsData = (data) => {
+    // console.log(data)
+    const artists = elementId('artists');
+    // console.log(artists)
+    data.artists.forEach((artist) => {
+        // console.log(artist)
+        const div = document.createElement('div');
+        div.classList.add("artist-card");
+        div.innerHTML = `
+        <div class="image-container">
+            <div class="image-container-inner">
+                <img src="${artist.strArtistThumb}" alt=""/>
+            </div>
+        </div>
+        <div class="info-container">
+            <h1>${artist.strArtist}</h1>
+            <p>Country: ${artist.strCountry}</p>
+            <p>Style: ${artist.strGenre}</p>
+        </div>
+        <button class="album-button">
+            <i class="fa-solid fa-compact-disc"></i>
+            <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+        </button>`;
+        artists.appendChild(div)
+    });
+};
